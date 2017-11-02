71324e6dcd2a6550a2a98e9a5b48207d92d43b31
nielsvd <nielsvanduijkeren@gmail.com>
2017-11-02 19:49:00 +0100

    First program layout.

---
+++ acados/interfaces/cpp/sim_solver.h
@@ -0,0 +1,31 @@
+/*
+ *    This file is part of acados.
+ *
+ *    acados is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    acados is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with acados; if not, write to the Free Software Foundation,
+ *    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef ACADOS_INTERFACES_CPP_SIM_SOLVER_H_
+#define ACADOS_INTERFACES_CPP_SIM_SOLVER_H_
+
+namespace acados {
+
+class SimSolver {
+
+};  // class SimSolver
+
+}  // namespace acados
+
+#endif  // ACADOS_INTERFACES_CPP_SIM_SOLVER_H_
